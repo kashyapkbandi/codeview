6abea1477ff9f6e27cf954422ab18fb3a74845e9
Kashyap Kalakbandi <kkalakbandi@internal.salesforce.com>
2020-09-27 21:03:05 +0530

    Fixed query exec

--- assets/scripts/UtilityScript.js
+++ assets/scripts/UtilityScript.js
@@ -2,22 +2,44 @@
 
 const OrgUtils = {
 
-    OrgBaseUrl:"https://codestation-dev-ed.my.salesforce.com",
-    isData:'/services/data/',
-    apiVersion:'v48.0',
-    queryString:'/query/?q='
+    OrgBaseUrl: "https://codestation-dev-ed.my.salesforce.com",
+    isData: '/services/data/',
+    apiVersion: 'v48.0',
+    queryString: '/query/?q='
 };
 
 
-document.querySelector('#executeButtn').addEventListener("click",()=>{
-// Event listener for the execute button
-// take the access token from the local storage session.
-let accessTokenValue= retrieveCookie(document.cookie);
+document.querySelector('#executeButtn').addEventListener("click", () => {
+    // Event listener for the execute button
+    // take the access token from the local storage session.
+    let accessTokenValue = retrieveCookie(document.cookie);
 
-// get query / code from the text area
-var query = document.querySelector('#codequeryTextArea').value;
+    // check if query checkbox is checked or not.
+ 
+    if (document.getElementById("soqlCheck").checked) {
+        
+        // If the SOQL is checked that means, the APEX should be unchecked
+        // Because we will be hitting diff endpoints to handle apex vs soql.
+        if (!document.getElementById("apexCheck").checked) {
+            // get query / code from the text area
+            var query = document.querySelector('#codequeryTextArea').value;
+            var result = fetchGETCall(OrgUtils.OrgBaseUrl + OrgUtils.isData + OrgUtils.apiVersion + OrgUtils.queryString + query, 'GET', '', accessTokenValue);
+        }
+        else {
+            alert("Please check either SOQL or APEX.");
+        }
+    } else
+    // this means, apex checkbox is on.
+    {
+        // if SOQL is unchecked or not
+        if (!document.getElementById("soqlCheck").checked) {
 
-fetchCall(OrgUtils.OrgBaseUrl+OrgUtils.isData+OrgUtils.apiVersion+OrgUtils.queryString+query,'GET','',accessTokenValue)
+        }
+        else {
+            // if apex and soql are checked
+            alert("Please check either SOQL or APEX.");
+        }
+    }
 
 
 
@@ -30,20 +52,28 @@ function retrieveCookie(cookie) {
 }
 
 
-function fetchCall(url,method,payload,accessToken)
-{
+function fetchGETCall(url, method, payload, accessToken) {
     fetch(url, {
         method: 'get',
         headers: {
-          "Content-type": "application/json",
-          "Authorization": "Bearer "+accessToken
+            "Content-type": "application/json",
+            "Authorization": "Bearer " + accessToken
         }
-      })
-    .then(response => response.json())
-.then(data => {
-console.log('Success:', data);
-})
-.catch((error) => {
-console.error('Error:', error);
-});
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            // prepare DOM element to display the result.
+            prepareDOMElement(data)
+
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
+
+function prepareDOMElement(result) {
+   document.getElementById("resultTextArea").value = '';
+   document.getElementById("resultTextArea").value = JSON.stringify(result);
+
 }
